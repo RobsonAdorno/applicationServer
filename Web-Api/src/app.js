d4962bf9dacb14ea2a831ba135174c96865998dc
Robson Adorno <rferreira@scopus.com.br>
2019-01-09 12:59:30 -0200

    Alterações

--- Web-Api/src/app.js
+++ Web-Api/src/app.js
@@ -1,21 +1,15 @@
 //Configuration for run the Web Server
 
 //import libs
-<<<<<<< HEAD
-var port = process.env.port || 3000;
-=======
 
->>>>>>> f8dba1caa41dd77e6930869b38cf21aa59aadeb3
+var port = process.env.port || 3000;
+
 const http = require("http");
 const express = require("express");
 
 //Basic Configuration
 
 const hostname = "127.0.0.1";
-<<<<<<< HEAD
-=======
-const port = "8081";
->>>>>>> f8dba1caa41dd77e6930869b38cf21aa59aadeb3
 
 //Initialize the server
 const app = express();
@@ -37,12 +31,9 @@ app.use(
 
 const server = http.createServer(app);
 
-<<<<<<< HEAD
 app.listen(
-=======
-server.listen(
->>>>>>> f8dba1caa41dd77e6930869b38cf21aa59aadeb3
-    port, hostname, () => {
+
+    port, () => {
 
         console.log("Server listening in the http://localhost:8081");
 
