30d3ed4e0e2c89bb91a8b84aee40dacfcd75b88e
Robson Adorno Ferreira <RobsAdorno@MacBook-Pro-de-Robson.local>
2019-01-10 05:58:01 -0200

    Mudando a porta da aplicação

--- Web-Api/src/app.js
+++ Web-Api/src/app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
 });
 
 // use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
+var port = process.env.port || 5000;
 
 app.listen(port);
