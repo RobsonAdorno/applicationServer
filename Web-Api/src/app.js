c565851da0c92ef21802ea462079c2a5c875dfb9
Robson Adorno Ferreira <RobsAdorno@MacBook-Pro-de-Robson.local>
2019-01-10 05:50:39 -0200

    Alterando configuracoes no Pocfile

--- Web-Api/src/app.js
+++ Web-Api/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("Teste");
 });
 
 // use port 3000 unless there exists a preconfigured port
