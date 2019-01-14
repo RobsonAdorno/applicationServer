e6a9e1d5e237bc7643acbaa7eaba2644e880aad3
Robson Adorno <robsonadorno_@hotmail.com>
2019-01-14 00:51:37 -0200

    Alterando configuracoes no json

---
+++ Web-Api/src/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+
+const port = 8081;
+const hostname = "127.0.0.1";
+
+const app = express();
+
+app.set("port", port);
+
+app.use((req, resp, next) => {
+
+  resp.status(200).send("Caralho mano, eh isso msm!!")
+
+});
+
+const server = http.createServer(app);
+
+server.listen( port, hostname, () => {
+
+  console.log("Servidor ouvindo em http://localhost:8081");
+}
+);
